76df20c50a0fcd65a99aaa856c73c6baf77d7cde
Charlie_egpu <tchoi.tab@gmail.com>
2022-01-09 23:18:30 +0900

    :sparkles: feat: 음악삭제 API

--- server/nodejs/source/app/controller/playlists/delete.js
+++ server/nodejs/source/app/controller/playlists/delete.js
@@ -3,23 +3,29 @@ const { Playlist } = require('../../models');
 const removePlaylist = async (req, res) => {
   if (typeof req.token === 'number') {
     console.log(
-      `[ERROR] /api/playlists DELETE -> req.token is missing or invalid`
+      `[ERROR] /api/playlists/:playlist_id DELETE -> req.token is missing or invalid`
     );
     return res.status(401).send('unauthorized');
   }
 
   if (!req.token['id']) {
-    console.log(`[ERROR] /api/playlists DELETE -> id does not exist`);
+    console.log(
+      `[ERROR] /api/playlists/:playlist_id DELETE -> id does not exist`
+    );
     return res.status(400).send('id does not exist');
   }
 
   if (isNaN(req.token.id) || req.token.id < 1 || req.token.id > 2147483647) {
-    console.log(`[ERROR] /api/playlists DELETE -> user id is invalid`);
+    console.log(
+      `[ERROR] /api/playlists/:playlist_id DELETE -> user id is invalid`
+    );
     return res.status(400).send('user id is invalid');
   }
 
   if (!req.params['playlist_id']) {
-    console.log(`[ERROR] /api/playlists DELETE -> playlist_id does not exist`);
+    console.log(
+      `[ERROR] /api/playlists/:playlist_id DELETE -> playlist_id does not exist`
+    );
     return res.status(400).send('playlist_id does not exist');
   }
 
@@ -29,7 +35,7 @@ const removePlaylist = async (req, res) => {
     req.params.playlist_id > 2147483647
   ) {
     console.log(
-      `[ERROR] /api/playlists DELETE -> playlist_id is invalid : ${req.params.playlist_id}`
+      `[ERROR] /api/playlists/:playlist_id DELETE -> playlist_id is invalid : ${req.params.playlist_id}`
     );
     return res.status(400).send('playlist_id is invalid');
   }
@@ -39,16 +45,20 @@ const removePlaylist = async (req, res) => {
   try {
     result = await Playlist.destroy({ where });
   } catch (err) {
-    console.log(`[ERROR] /api/playlists DELETE -> 500 : ${err}`);
+    console.log(`[ERROR] /api/playlists/:playlist_id DELETE -> 500 : ${err}`);
     return res.status(500).send('Internal Server Error');
   }
-  console.dir(result);
+
   if (!result) {
-    console.log(`[ERROR] /api/playlists DELETE -> 404 : delete failed`);
+    console.log(
+      `[ERROR] /api/playlists/:playlist_id DELETE -> 404 : delete failed`
+    );
     return res.status(404).send('delete failed');
   }
 
-  console.log(`[SUCCESS] /api/playlists DELETE -> 204 : delete success`);
+  console.log(
+    `[SUCCESS] /api/playlists/:playlist_id DELETE -> 204 : delete success`
+  );
   return res.status(204).send();
 };
 
