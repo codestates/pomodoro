152df0562bb72a7448446670928b681b6b1297a2
Kae <kaehehehe@gmail.com>
2022-01-10 13:02:57 +0900

    :sparkles:feat: UI 구현 및 플레이리스트 이름 변경 기능 구현

---
+++ client/src/components/desktop/MyPlaylist.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { SectionContainer } from '../../styles/SectionContainer';
+import Playlist from '../../components/desktop/Playlist';
+import styled from 'styled-components';
+
+import dummyData from '../../dummyData/myPlaylist';
+
+const Container = styled(SectionContainer)`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: space-between;
+  height: 350px;
+  padding: 2em;
+  font-size: 1.5rem;
+`;
+
+const Playlists = styled.ol`
+  width: 100%;
+  overflow-y: scroll;
+`;
+
+const Title = styled.h3`
+  width: 100%;
+  padding-bottom: 10px;
+  border-bottom: 1px solid black;
+  margin-bottom: 10px;
+`;
+
+const MyPlaylist = () => {
+  return (
+    <Container>
+      <Title>내 플레이리스트</Title>
+      <Playlists>
+        {dummyData.map((item) => (
+          <Playlist
+            key={item.playlist_id}
+            order={item.playlist_id}
+            name={item.playlist_name}
+          />
+        ))}
+      </Playlists>
+    </Container>
+  );
+};
+
+export default MyPlaylist;
