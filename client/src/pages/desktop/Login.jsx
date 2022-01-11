2c738f788f89d7eed4b1d36256f6070c3a8b5668
Kae <kaehehehe@gmail.com>
2022-01-11 11:50:07 +0900

    :gear:wip: 로그인 기능 구현

--- client/src/pages/desktop/Login.jsx
+++ client/src/pages/desktop/Login.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FormContainer } from '../../styles/FormContainer.styled';
 import { Form } from '../../styles/Form.styled';
 import { FormInput } from '../../styles/FormInput.styled';
 import { FormWrapper } from '../../styles/FormWrapper.styled';
 import { FormBtn } from '../../styles/FormBtn.styled';
 import { FormText } from '../../styles/FormText.styled';
+import { FormErrorMsg } from '../../styles/FormErrorMsg.styled';
 import { ReactComponent as Logo } from '../../images/logo.svg';
 import styled from 'styled-components';
+import axios from 'axios';
 
 const Wrapper = styled.ul`
   width: 90%;
@@ -23,23 +26,58 @@ const Wrapper = styled.ul`
 `;
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [showErrMsg, setShowErrMsg] = useState(false);
+  const emailRef = useRef('');
+  const pwRef = useRef('');
+
+  useEffect(() => {
+    emailRef.current.focus();
+  }, []);
+
+  const handleLoginBtn = async () => {
+    let result;
+    try {
+      result = await axios.post(
+        'https://final.eax.kr/api/auth',
+        {
+          nickname: emailRef.current.value,
+          password: pwRef.current.value,
+        },
+        { withCredentials: true }
+      );
+    } catch (error) {
+      setShowErrMsg(true);
+    }
+    if (result) {
+      navigate('/');
+    }
+    emailRef.current.value = '';
+    pwRef.current.value = '';
+  };
+
   return (
     <FormContainer>
       <Form>
-        <Logo />
+        <Logo onClick={() => navigate('/')} />
         <FormWrapper marginTop="40px" marginBottom="30px">
-          <FormInput type="text" placeholder="이메일" />
-        </FormWrapper>
-        <FormWrapper marginBottom="30px">
-          <FormInput type="password" placeholder="비밀번호" />
+          <FormInput type="email" ref={emailRef} placeholder="이메일" />
         </FormWrapper>
         <FormWrapper>
-          <FormText>비밀번호를 잊으셨나요?</FormText>
+          <FormInput type="password" ref={pwRef} placeholder="비밀번호" />
         </FormWrapper>
-        <FormBtn>로그인</FormBtn>
+        <FormErrorMsg show={showErrMsg}>
+          이메일과 비밀번호가 일치하지 않습니다.
+        </FormErrorMsg>
+        <FormWrapper>
+          <FormText onClick={() => navigate('/forgotpw')}>
+            비밀번호를 잊으셨나요?
+          </FormText>
+        </FormWrapper>
+        <FormBtn onClick={handleLoginBtn}>로그인</FormBtn>
         <Wrapper>
           <li>아직 회원이 아니세요?</li>
-          <li>회원 가입</li>
+          <li onClick={() => navigate('/signup')}>회원 가입</li>
         </Wrapper>
       </Form>
     </FormContainer>
