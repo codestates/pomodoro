5d48afdf11d5ba99cb07dfa0904843442b0ebf0a
krim45 <krim45@naver.com>
2022-01-12 20:37:50 +0900

    :sparkles:feat: LandingPage 컴포넌트 - text 애니메이션 추가 - semantic tag 수정

--- client/src/components/desktop/LandingPage.jsx
+++ client/src/components/desktop/LandingPage.jsx
@@ -8,14 +8,15 @@ import img3 from '../../images/img3.png';
 import img4 from '../../images/img4.png';
 import img5 from '../../images/img5.png';
 import img6 from '../../images/img6.png';
+import ScrollToTop from './ScrollToTop';
 
-const Wrapper = styled.div`
+const Wrapper = styled.main`
   max-width: 1320px;
   margin: 0 auto;
   padding: 3rem;
 
-  > div {
-    padding: 6rem 0;
+  > section {
+    padding: 12rem 0;
     display: grid;
     grid-template-columns: 1fr 1fr;
     align-content: center;
@@ -66,23 +67,43 @@ const Wrapper = styled.div`
       margin-bottom: 5rem;
     }
 
-    div.text {
+    .text {
       font-size: 3rem;
       font-weight: 700;
       text-align: center;
       line-height: 7rem;
       margin-bottom: 3rem;
     }
+
+    button {
+      background-color: #f57270;
+      border: none;
+      color: #ffffff;
+      font-size: 3rem;
+      font-weight: 700;
+      padding: 2.5rem 4.5rem;
+      border-radius: 50px;
+    }
   }
 
-  button {
-    background-color: #f57270;
-    border: none;
-    color: #ffffff;
-    font-size: 3rem;
-    font-weight: 700;
-    padding: 2.5rem 4.5rem;
-    border-radius: 50px;
+  .up {
+    opacity: 0;
+    transform: translateY(25px);
+    transition: 0.3s linear;
+  }
+
+  .down {
+    opacity: 0;
+    transform: translateY(-25px);
+    transition: 0.3s linear;
+  }
+
+  .text-box-left {
+    padding-left: 15rem;
+  }
+
+  .text-box-right {
+    padding-right: 15rem;
   }
 
   .text-left {
@@ -101,12 +122,40 @@ const LandingPage = () => {
   const box4 = useRef(null);
   const box5 = useRef(null);
   const box6 = useRef(null);
+  const textBox1 = useRef(null);
+  const textBox2 = useRef(null);
+  const textBox3 = useRef(null);
+  const textBox4 = useRef(null);
+  const textBox5 = useRef(null);
+  const textBox6 = useRef(null);
 
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
     const boxArray = [box1, box2, box3, box4, box5, box6];
-    let animation1, animation2, animation3, animation4, animation5, animation6;
-    const animations = [
+
+    const textBoxArray = [
+      textBox1,
+      textBox2,
+      textBox3,
+      textBox4,
+      textBox5,
+      textBox6,
+    ];
+
+    let animation1,
+      animation2,
+      animation3,
+      animation4,
+      animation5,
+      animation6,
+      animation7,
+      animation8,
+      animation9,
+      animation10,
+      animation11,
+      animation12;
+
+    const boxAnimations = [
       animation1,
       animation2,
       animation3,
@@ -115,13 +164,21 @@ const LandingPage = () => {
       animation6,
     ];
 
+    const textBoxAnimations = [
+      animation7,
+      animation8,
+      animation9,
+      animation10,
+      animation11,
+      animation12,
+    ];
+
     boxArray.forEach((box, idx) => {
-      animations[idx] = gsap.to(box.current, {
+      boxAnimations[idx] = gsap.to(box.current, {
         scrollTrigger: {
           trigger: box.current,
           toggleActions: 'play reset restart reset',
-          markers: true,
-          start: 'center 650px',
+          start: 'top 650px',
           end: 'bottom 150px',
         },
         ease: 'elastic',
@@ -130,40 +187,47 @@ const LandingPage = () => {
       });
     });
 
-    return () => {
-      animations.forEach((animation) => {
-        animation.kill();
+    textBoxArray.forEach((textBox, idx) => {
+      textBoxAnimations[idx] = gsap.to(textBox.current, {
+        scrollTrigger: {
+          trigger: textBox.current,
+          toggleActions: 'play reset restart reset',
+          start: 'top 650px',
+          end: 'bottom 150px',
+        },
+        ease: 'elastic',
+        opacity: 1,
+        y: 0,
       });
+    });
+
+    return () => {
+      boxAnimations.forEach((animation) => animation.kill());
+      textBoxAnimations.forEach((animation) => animation.kill());
     };
   }, []);
 
   return (
     <Wrapper>
-      <div>
+      <section>
         <div className="box">
-          <img
-            className="box1 left"
-            src={img1}
-            alt="landing img1"
-            ref={box1}
-          ></img>
+          <img className="box1 left" src={img1} alt="landing img1" ref={box1} />
         </div>
-        <div>
+        <div className="text-box-left up" ref={textBox1}>
           <h1 className="text-left">집중하고 계신가요?</h1>
           <p className="text-left">
             COVID-19으로 재택근무자와 취준생이 늘었습니다.
             <br />
-            하지만 휴식의 공간인 집에서 휴식 외의 일을
-            <br />
-            한다는 것은 강한 의지를 요구하는 일입니다.
+            하지만 휴식의 공간인 집에서 휴식 외의 일을 한다는 건 강한 의지를
+            요구하는 일입니다.
             <br />
             집중을 도와줄 Pomodoro 사용해보세요!
           </p>
         </div>
-      </div>
+      </section>
 
-      <div>
-        <div>
+      <section>
+        <div className="text-box-right up" ref={textBox2}>
           <h1 className="line-height text-right">
             25분 집중
             <br />
@@ -185,21 +249,16 @@ const LandingPage = () => {
             src={img2}
             alt="landing img2"
             ref={box2}
-          ></img>
+          />
         </div>
-      </div>
+      </section>
 
-      <div>
+      <section>
         <div className="box">
-          <img
-            className="box3 left"
-            src={img3}
-            alt="landing img3"
-            ref={box3}
-          ></img>
+          <img className="box3 left" src={img3} alt="landing img3" ref={box3} />
         </div>
         <div>
-          <h1 className="line-height">
+          <h1 className="line-height up" ref={textBox3}>
             뽀모도로로
             <br />
             시간을
@@ -207,11 +266,11 @@ const LandingPage = () => {
             관리해보세요
           </h1>
         </div>
-      </div>
+      </section>
 
-      <div>
+      <section>
         <div>
-          <h1 className="line-height">
+          <h1 className="line-height up" ref={textBox4}>
             Youtube와 함께
             <br />
             원하는 음악으로
@@ -223,21 +282,16 @@ const LandingPage = () => {
             src={img4}
             alt="landing img4"
             ref={box4}
-          ></img>
+          />
         </div>
-      </div>
+      </section>
 
-      <div>
+      <section>
         <div className="box">
-          <img
-            className="box5 left"
-            src={img5}
-            alt="landing img5"
-            ref={box5}
-          ></img>
+          <img className="box5 left" src={img5} alt="landing img5" ref={box5} />
         </div>
         <div>
-          <h1 className="line-height">
+          <h1 className="line-height up" ref={textBox5}>
             랭킹을 통해
             <br />
             다른 유저와
@@ -245,10 +299,10 @@ const LandingPage = () => {
             경쟁도 가능해요
           </h1>
         </div>
-      </div>
+      </section>
 
-      <div>
-        <div className="landing-6">
+      <section>
+        <div className="landing-6 up" ref={textBox6}>
           <div className="text text-right">
             뽀모도로를 이용해
             <br />
@@ -271,9 +325,10 @@ const LandingPage = () => {
             src={img6}
             alt="landing img 6"
             ref={box6}
-          ></img>
+          />
         </div>
-      </div>
+      </section>
+      <ScrollToTop />
     </Wrapper>
   );
 };
