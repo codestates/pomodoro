28f08ed38b6e1274c5a03fb14331640ca0b83d0c
krim45 <krim45@naver.com>
2022-01-14 04:15:10 +0900

    :art:style: Footer CSS 수정 - 전체적인 크기 축소 - 아이콘 색 연하게 조정 - 폰트 색상 변경 - 스타일컴포넌트 정리

--- client/src/components/desktop/Footer.jsx
+++ client/src/components/desktop/Footer.jsx
@@ -7,71 +7,72 @@ const Foot = styled.footer`
   justify-content: space-around;
   align-items: flex-end;
   max-width: 1320px;
-  padding: 0.75rem 0;
+  padding: 2rem 0;
   margin: 0 auto;
+`;
 
-  > a {
-    width: 11.5rem;
-    /* margin: 0 5.25rem; */
-    display: flex;
-    justify-content: space-around;
-  }
-
-  img {
-    width: 3.5rem;
-    height: 3.5rem;
-  }
+const GithubLink = styled.a`
+  width: 9rem;
+  display: flex;
+  justify-content: space-around;
 
   li {
-    font-size: 1.2rem;
+    color: #111032;
+    font-size: 1rem;
     font-weight: 700;
     text-align: center;
     margin: 0;
     padding: 0;
 
     :first-child {
-      font-size: 2.2rem;
+      font-size: 1.5rem;
     }
   }
 
   > div {
-    /* margin: 0 5.25rem; */
-    font-size: 1.4rem;
+    color: #111032;
+    font-size: 1.2rem;
     font-family: 'Poppins', sans-serif;
   }
+`;
+
+const Img = styled.img`
+  width: 3rem;
+  height: 3rem;
+  opacity: 0.6;
 `;
 
 const Footer = () => {
   return (
     <Foot>
-      <a href="https://github.com/krim45">
-        <img src={github}></img>
+      <GithubLink href="https://github.com/krim45">
+        <Img src={github}></Img>
         <ul>
           <li>김경봉</li>
           <li>front-end</li>
         </ul>
-      </a>
-      <a href="https://github.com/kaehehehe">
-        <img src={github}></img>
+      </GithubLink>
+      <GithubLink href="https://github.com/kaehehehe">
+        <Img src={github}></Img>
         <ul>
           <li>카에</li>
           <li>front-end</li>
         </ul>
-      </a>
-      <a href="https://github.com/exxocism">
-        <img src={github}></img>
+      </GithubLink>
+      <GithubLink href="https://github.com/exxocism">
+        <Img src={github}></Img>
         <ul>
           <li>최민우</li>
           <li>full-stack</li>
         </ul>
-      </a>
-      <a href="https://github.com/AbyulStudy">
-        <img src={github}></img>
+      </GithubLink>
+      <GithubLink href="https://github.com/AbyulStudy">
+        <Img src={github}></Img>
         <ul>
           <li>최민석</li>
           <li>back-end</li>
         </ul>
-      </a>
+      </GithubLink>
       <div>©2022 Buglife</div>
     </Foot>
   );
