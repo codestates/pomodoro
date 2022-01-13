15460c3eae4ec9d044451ca5c8de2186ae3615c6
Kae <kaehehehe@gmail.com>
2022-01-13 19:58:03 +0900

    :sparkles:feat: 모바일 UI 구현

--- client/src/pages/desktop/MyPage.jsx
+++ client/src/pages/desktop/MyPage.jsx
@@ -5,13 +5,19 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
   width: 100vw;
-  height: 80vh;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   color: #945151;
   user-select: none;
+
+  @media screen and (max-width: 768px) {
+    margin-top: 100px;
+    margin-bottom: 100px
+  }
 `;
 
 const MyPage = ({ userInfo, playlist, setPlaylist }) => {
