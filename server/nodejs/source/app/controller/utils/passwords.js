a79c56f6512bffc5ec7862d01f385b99bc5c1817
Abyul <kinsng1599@gmail.com>
2022-01-16 21:57:26 +0900

    :sparkles:feat: 비밀번호 찾기 메일전송 API body에 email 값을 받아 온 이후 받아온 email 과 인증 메일 보내기 API 와 동일하게 임의의 데이터를 작성하여 쓰레기 값을 payload에 추가 햇습니다. 토큰은 10분짜리로 발급 됩니다.

--- server/nodejs/source/app/controller/utils/passwords.js
+++ server/nodejs/source/app/controller/utils/passwords.js
@@ -1,13 +1,53 @@
+const { mailTranport } = require('../../config/email');
+require('dotenv').config();
+const ejs = require('ejs');
+const { createToken } = require('./tokenFunctions');
+const crypto = require('crypto');
+const { mailSendFunction, ejsRenderFile } = require('./mailFunctions');
+const { User } = require('../../models/');
+const { checkInputData, findUserInfomation } = require('./error/error');
+
 const sendEmailToRetrievePassword = async (req, res) => {
-  const stub = `[stub] /api/passwords POST`;
-  console.log(stub);
-  res.status(200).send(stub);
+  const path = `/api/passwords POST`;
+  const stub = `sendEmailToRetrievePassword`;
+  console.log(`[stub] ${path} ${stub}`);
+
+  checkInputData(res, [req.body['email']]);
+  const { email } = req.body;
+
+  const findUser = await User.findOne({ where: { email } }).then((user) => {
+    findUserInfomation(res, user);
+    const rand = crypto.randomBytes(64).toString('hex');
+    const user_id = user.getDataValue('user_id');
+    const payload = {
+      auth_id: user_id,
+      salt: rand,
+    };
+    const authToken__10m = createToken(payload, '10m');
+
+    const path = '/source/app/view/passwordMail.ejs';
+    const purpose = 'forgetPassword';
+    const emailTemplate = ejsRenderFile(res, purpose, path, {
+      code: authToken__10m,
+      apiUrl: process.env.GMAIL_PASSWORDMAIL,
+    });
+
+    const mailSubject = '[POMODORO] 비밀번호 찾기 메일입니다.';
+    const mailOption = {
+      from: process.env.GMAIL_USER,
+      to: email,
+      subject: mailSubject,
+      html: emailTemplate,
+    };
+
+    const mailSend = mailSendFunction(res, mailOption, authToken__10m);
+  });
 };
 
 const changeUserPassword = async (req, res) => {
-  const stub = `[stub] /api/passwords PATCH`;
-  console.log(stub);
-  res.status(200).send(stub);
+  const path = `/api/passwords PATCH`;
+  const stub = `changeUserPassword`;
+  console.log(`[stub] ${path} ${stub}`);
 };
 
 module.exports = {
