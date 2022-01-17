34acd4f8fb105d066ef62ce3f91d13c91cc88272
Charlie_mac <tchoi.tab@gmail.com>
2022-01-18 08:54:35 +0900

    :bug: fix: 백엔드 라우팅 문제 수정

--- server/nodejs/source/app/router/playlists.js
+++ server/nodejs/source/app/router/playlists.js
@@ -4,7 +4,7 @@ const controller = require('../controller');
 //PLAYLIST
 router.get('/', controller.playlists.info);
 router.post('/', controller.playlists.addItem);
-router.put('/:playlist_id', controller.playlists.orderChange); // advanced
+router.put('/', controller.playlists.orderChange); // advanced
 router.patch('/:playlist_id', controller.playlists.modifyName);
 router.delete('/:playlist_id', controller.playlists.delete);
 
