7839aac887439dc6c4de9d624d63cc1b538be67f
Abyul <kinsng1599@gmail.com>
2022-01-19 20:34:08 +0900

    :sparkles:feat: youtube API router 작성

--- server/nodejs/source/app/router/youtube.js
+++ server/nodejs/source/app/router/youtube.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const controller = require('../controller');
 
 //AUTH
-router.post('/:playlist_id', controller.youtube.test);
+router.post('/:playlist_id', controller.youtube.saveMusicAndThumbnail);
 
 module.exports = router;
