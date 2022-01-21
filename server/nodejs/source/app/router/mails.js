7b8b9bf8b64eccdadb5b4e815bc46a75f10c1682
Abyul <kinsng1599@gmail.com>
2022-01-21 18:16:17 +0900

    :bug:bug: pending update 라우터 patch 로 변경 및 주석 제거

--- server/nodejs/source/app/router/mails.js
+++ server/nodejs/source/app/router/mails.js
@@ -4,6 +4,6 @@ const controller = require('../controller');
 //MAIL
 router.get('/:email', controller.mails.checkValued);
 router.post('/', controller.mails.sendAuthMail);
-// router.post('/:user_id', controller.mails.pendingUpdate);
+router.patch('/', controller.mails.pendingUpdate);
 
 module.exports = router;
